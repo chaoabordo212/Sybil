1059b6ea5e1a363b5781aa17c8533e13269d41d3
united <chaoabordo@gmail.com>
2021-01-24 05:27:50 +0100

    01/24/21 05:27

--- components/config_db.py
+++
@@ -1,7 +0,0 @@
-mongodb_dbname = "spadeDB" # DB Name
-mongodb_admindb = 'admin' # Admin DB
-#dbhost = '192.168.1.104' # Spade mongodb serv
-mongodb_host = '192.168.1.199' # mongodb localhost serv
-mongodb_port = 27017 # DB port
-mongodb_user = 'spadeUser' # DB Username
-mongodb_pass = "S1EdE9xxxxAuIII@#!22Dandjop" # DB Password
