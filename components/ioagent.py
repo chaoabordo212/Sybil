feef2ebe26c69f6408edaaea8a99e58ffb3a7998
united <chaoabordo@gmail.com>
2021-02-01 17:14:48 +0100

    02/01/21 17:14

--- components/ioagent.py
+++ components/ioagent.py
@@ -6,16 +6,31 @@ import traceback
 from .dbagent import *
 from .config_db import *
 
-
-def query_lastqnum(dict1_collection_sortfield):
-    arg1_collection, arg2_sortfield = dict1_collection_sortfield['collection'], dict1_collection_sortfield['sortfield']
+def query_lastnum(arg1_collection_sortfield_showfield_dict):
+    var1_collection, var2_sortfield, var3_showfield = arg1_collection_sortfield_showfield_dict['collection'], arg1_collection_sortfield_showfield_dict['findmany'], arg1_collection_sortfield_showfield_dict['showfield']
     try:
-        lastqnum = db_findlast(arg1_collection, arg2_sortfield)['Num']
-        return lastqnum
+        lastnum = db_findlast(var1_collection, var2_sortfield)[var3_showfield]
+        return lastnum
     except Exception as err:
         ioagent_trace = traceback.print_exception(type(err), err, err.__traceback__)
         return ioagent_trace
-        return "lastqnum_error"
+        return "lastnum_error"
+
+def query_list(arg1_collection_findmany_dict):
+    var1_collection, var2_findmany_dict = arg1_collection_findmany_dict['collection'], arg1_collection_findmany_dict['findmany']
+    try:
+        query_found_list = db_findmany(var1_collection, var2_findmany_dict)
+        return query_found_list
+    except Exception as err:
+        ioagent_trace = traceback.print_exception(type(err), err, err.__traceback__)
+        return ioagent_trace
+        return "list_error"
+
+#def query_find():
+#
+#def query_add():
+#
+#def query_remove():
 
 # def query_input(arg1_query)
 
---
+++ queryman.py
@@ -0,0 +1,6 @@
+# queryman.py
+# Query management - list, find, add, remove, start 
+
+# def query_management_list(arg1_list_dict):
+# 	var1_collection, var2_findmany_dict = arg1_list_dict['']
+	
